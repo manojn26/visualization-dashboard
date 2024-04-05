9bf512a6ac45f50f290b5ef7b180a606ab7046ab
Manoj Kumar N <manojcsengineer@gmail.com>
2024-04-05 06:46:06 +0530

    all-functionalities-wokring-perfectly

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./components/Dashboard/Main";
-import Login from "./components/Login/Login";
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/dashboard" element={<Main />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
+    <>
+      <Main />
+    </>
   );
 };
 
--- client/src/components/Dashboard/Footer.jsx
+++ client/src/components/Dashboard/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
         px={4}
       >
         <Text fontSize="sm" color="gray.500">
-          &copy; 2023 Ritesh Kr. Shukla. All rights reserved.
+          &copy; 2024 Manoj Kumar N. All rights reserved.
         </Text>
         <Flex alignItems="center">
           <Link mx={2} fontSize="sm" color="gray.500">
